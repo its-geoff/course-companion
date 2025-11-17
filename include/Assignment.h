f7f65069531a6208f8f31f3cf87831e76dc9c8c3
its-geoff <geoffrey.agustin04@gmail.com>
2025-11-16 23:01:46 -0800

    added constructors, print, setter, and getter functions

--- include/Assignment.h
+++ include/Assignment.h
@@ -3,7 +3,7 @@
 
 // -------------------------------------------------------------------------------------------------------------------
 // File: Assignment.h
-// Description:
+// description_:
 //      Definition of an Assignment class that stores metadata and is used to organize coursework.
 //      Provides declarations only; see Assignment.cpp for implementations.
 // -------------------------------------------------------------------------------------------------------------------
@@ -15,18 +15,35 @@
  * @class Assignment
  * @brief Represents an academic assignment given to a student.
  * 
- * Stores metadata such as title, description, due date, completion status, and grade.
+ * Stores metadata such as title_, description_, due date, completion status, and grade_.
  * The Assignment class is used by the main function to track progress and report assignment results.
  */
 class Assignment {
     private:
-        std::string title{};
-        std::string description{};
-        std::chrono::system_clock::time_point due_date;
-        bool completion_status{false};
-        float grade{0.0};
+        std::string title_{};
+        std::string description_{};
+        std::chrono::system_clock::time_point dueDate{};
+        bool completionStatus_{false};
+        float grade_{0.0};
 
     public:
+        Assignment();
+        Assignment(std::string title_, std::chrono::system_clock::time_point dueDate_);
+        Assignment(std::string title_, std::chrono::system_clock::time_point dueDate_, bool completionStatus_);
+        Assignment(std::string title_, std::chrono::system_clock::time_point dueDate_, bool completionStatus_, float grade_);
+
+        void printAssignment();
+
+        void setTitle(std::string_view newTitle);
+        void setDescription(std::string_view newDescription);
+        void setDueDate(std::chrono::system_clock::time_point newDueDate);
+        void setCompletionStatus(bool newCompletionStatus);
+        void setGrade(float newGrade);
+        std::string getTitle();
+        std::string getDescription();
+        std::chrono::system_clock::time_point getDueDate();
+        bool getCompletionStatus();
+        float getGrade();
 };
 
 #endif
