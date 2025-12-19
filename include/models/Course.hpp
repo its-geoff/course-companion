41fdf40a6c78a24366cbe98d4aed7de40688fc8e
its-geoff <geoffrey.agustin04@gmail.com>
2025-12-18 19:51:09 -0800

    created CourseTests and added file to testing paths, added Course getter and setter functions

--- include/models/Course.hpp
+++ include/models/Course.hpp
@@ -29,44 +29,49 @@ class Course {
     private:
         std::string title_{};
         std::string description_{};
-        std::chrono::system_clock::time_point startDate_{};
-        std::chrono::system_clock::time_point endDate_{};
+        std::chrono::year_month_day startDate_{};
+        std::chrono::year_month_day endDate_{};
         std::vector<Assignment> assignmentList_{};
         int numCredits_{0};
         std::map<std::string, float> gradingScale_{};
-        std::string grade_{};
+        float gradePct_{0.0};    // grade percentage from 0 to 100%
+        std::string letterGrade_{};
         bool active_{true};     // indicates whether the course is currently ongoing
 
     public:
-        Course();
-        Course(std::string title_, std::chrono::system_clock::time_point startDate_, std::chrono::system_clock::time_point endDate_);
-        Course(std::string title_, std::chrono::system_clock::time_point startDate_, std::chrono::system_clock::time_point endDate_,
-                int numCredits_);
-        Course(std::string title_, std::chrono::system_clock::time_point startDate_, std::chrono::system_clock::time_point endDate_,
-                int numCredits_, bool active_);
-        Course(std::string title_, std::string description_, std::chrono::system_clock::time_point startDate_, 
-                std::chrono::system_clock::time_point endDate_);
-        Course(std::string title_, std::string description_, std::chrono::system_clock::time_point startDate_, 
-            std::chrono::system_clock::time_point endDate_, int numCredits_);
-        Course(std::string title_, std::string description_, std::chrono::system_clock::time_point startDate_, 
-            std::chrono::system_clock::time_point endDate_, int numCredits_, bool active_);
+        Course(std::string title, std::chrono::year_month_day startDate, std::chrono::year_month_day endDate);
+        Course(std::string title, std::chrono::year_month_day startDate, std::chrono::year_month_day endDate,
+                int numCredits);
+        Course(std::string title, std::chrono::year_month_day startDate, std::chrono::year_month_day endDate,
+                int numCredits, bool active);
+        Course(std::string title, std::string description, std::chrono::year_month_day startDate, 
+                std::chrono::year_month_day endDate);
+        Course(std::string title, std::string description, std::chrono::year_month_day startDate, 
+            std::chrono::year_month_day endDate, int numCredits);
+        Course(std::string title, std::string description, std::chrono::year_month_day startDate, 
+            std::chrono::year_month_day endDate, int numCredits, bool active);
 
-        void printCourseInfo();
-
-        void setTitle(std::string_view newTitle);
-        void setDescription(std::string_view newDescription);
-        void setStartDate(std::chrono::system_clock::time_point newStartDate);
-        void setEndDate(std::chrono::system_clock::time_point newEndDate);
-        void setNumCredits(int newNumCredits);
-        void setGradingScale(std::map<std::string, float> newGradingScale);
-        void setActive(bool newActive);
         std::string_view getTitle();
         std::string_view getDescription();
-        std::chrono::system_clock::time_point getStartDate();
-        std::chrono::system_clock::time_point getEndDate();
+        std::chrono::year_month_day getStartDate();
+        std::chrono::year_month_day getEndDate();
         int getNumCredits();
         std::map<std::string, float> getGradingScale();
+        float getGradePct();
+        std::string getLetterGrade();
         bool getActive();
+        void setTitle(std::string newTitle);
+        void setDescription(std::string newDescription);
+        void setStartDate(std::chrono::year_month_day newStartDate);
+        void setEndDate(std::chrono::year_month_day newEndDate);
+        void setNumCredits(int newNumCredits);
+        void setGradePct(float newGradePct);
+        void setLetterGrade(std::string newLetterGrade);
+        void setGradingScale(std::map<std::string, float> newGradingScale);
+        void setActive(bool newActive);
+
+        void calculateLetterGrade();
+        void printCourseInfo();
         void addAssignment();
         void removeAssignment();
 };
