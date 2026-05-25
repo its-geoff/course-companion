4981df34ce505217fce7d2e18a1686602ead3e6f
its-geoff <109061107+its-geoff@users.noreply.github.com>
2026-05-24 23:35:23 -0700

    refactored database.py to be more readable

--- server/app/database.py
+++ server/app/database.py
@@ -1,24 +1,37 @@
+from collections.abc import Generator
 import os
 
 import pymysql
-import pymysql.cursors
 from dotenv import load_dotenv
+from pymysql.connections import Connection
+from pymysql.cursors import DictCursor
 
 load_dotenv()
 
 
-def get_db():
-    conn = pymysql.connect(
+def get_db() -> Generator[Connection[DictCursor], None, None]:
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_name = os.getenv("DB_NAME")
+
+    if not db_user:
+        raise ValueError("DB_USER not set")
+
+    if not db_password:
+        raise ValueError("DB_PASSWORD not set")
+
+    if not db_name:
+        raise ValueError("DB_NAME not set")
+
+    conn: Connection[DictCursor] = pymysql.connect(
         host=os.getenv("DB_HOST") or "localhost",
         port=int(os.getenv("DB_PORT") or 3306),
-        user=os.getenv("DB_USER")
-        or (_ for _ in ()).throw(ValueError("DB_USER not set")),
-        password=os.getenv("DB_PASSWORD")
-        or (_ for _ in ()).throw(ValueError("DB_PASSWORD not set")),
-        database=os.getenv("DB_NAME")
-        or (_ for _ in ()).throw(ValueError("DB_NAME not set")),
-        cursorclass=pymysql.cursors.DictCursor,
+        user=db_user,
+        password=db_password,
+        database=db_name,
+        cursorclass=DictCursor,
     )
+
     try:
         yield conn
     finally:
